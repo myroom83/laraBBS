34d3d329184bd2c7602358d22cd5105708f80296
jeff sun <jeff.myroom83@gmail.com>
2019-03-02 21:29:16 +0800

    注册验证码

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,7 +24,7 @@
                                 @endif
                             </div>
                         </div>
-
+                        {{--邮箱输入--}}
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -38,7 +38,7 @@
                                 @endif
                             </div>
                         </div>
-
+                        {{--密码输入--}}
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -52,13 +52,29 @@
                                 @endif
                             </div>
                         </div>
-
+                        {{--确认密码--}}
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
+                        </div>
+
+                        <div class="form-group row">
+                          <label for="captcha" class="col-md-4 col-form-label text-md-right">验证码</label>
+
+                          <div class="col-md-6">
+                            <input id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha" required>
+
+                            <img class="thumbnail captcha mt-3 mb-2" src="{{ captcha_src('default') }}" onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片重新获取验证码">
+
+                            @if ($errors->has('captcha'))
+                              <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('captcha') }}</strong>
+                              </span>
+                            @endif
+                          </div>
                         </div>
 
                         <div class="form-group row mb-0">
