ddeef3dea3d7a2e50c74c0b606a68199d1b23ef1
jeff sun <jeff.myroom83@gmail.com>
2019-03-04 01:47:31 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+
 
 class UsersController extends Controller
 {
@@ -13,13 +15,14 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-//    public function edit()
-//    {
-//        return "hello edit";
-//    }
-//
-//    public function update()
-//    {
-//        return "hello update";
-//    }
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
 }
