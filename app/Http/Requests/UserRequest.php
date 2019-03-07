d8e01d32a878fcabf547e5f93101818ab3886cf2
jeff sun <jeff.myroom83@gmail.com>
2019-03-08 00:12:34 +0800

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,12 +28,16 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mines:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208'
+
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mines' => '头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensiongs' => '图片的清晰度不够，宽和高需要208px 以上',
             'name.unique' => '用户名 已被占用，请重新填写',
             'name.regex' => '用户名 只支持英文、数字、横杠和下划线。',
             'name.between' => '用户名 必须介于 3 - 25 个字符之间。',
