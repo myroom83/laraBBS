6849f9f4fe03d83b75277313874e3d51c0392dc2
jeff sun <jeff.myroom83@gmail.com>
2019-03-09 17:09:53 +0800

    话题排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,11 +14,18 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-		$topics = Topic::with('user', 'category')->paginate(30);
-		return view('topics.index', compact('topics'));
-	}
+//	public function index()
+//	{
+//		$topics = Topic::with('user', 'category')->paginate(30);
+//		return view('topics.index', compact('topics'));
+//	}
+
+    // 重写index() 方法
+    public function index(Request $request, Topic $topic)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        return view('topics.index', compact('topics'));
+    }
 
     public function show(Topic $topic)
     {
