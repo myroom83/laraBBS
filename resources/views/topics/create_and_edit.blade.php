96742b04f3d0159f002c60aef8364d5cbcced8ef
jeff sun <jeff.myroom83@gmail.com>
2019-03-10 17:37:48 +0800

    用户可以编辑话题

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -32,11 +32,14 @@
           <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title) }}" placeholder="请填写标题" required />
         </div>
 
+        {{-- 选择分类 --}}
         <div class="form-group">
           <select class="form-control" name="category_id" required>
-            <option value="" hidden disabled selected>请选择分类</option>
+            <option value="" hidden disabled {{ $topic->id ? '' : selected }}>请选择分类</option>
             @foreach($categories as $value)
-              <option value="{{ $value->id }}">{{ $value->name }}</option>
+              <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
+                {{ $value->name }}
+              </option>
             @endforeach
           </select>
         </div>
