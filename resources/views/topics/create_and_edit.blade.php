1794f1f46d8cfcdc3be37876898ba0fbd0b4eb0e
jeff sun <jeff.myroom83@gmail.com>
2019-03-11 13:56:27 +0800

    翻译 URL 标示

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -35,7 +35,7 @@
         {{-- 选择分类 --}}
         <div class="form-group">
           <select class="form-control" name="category_id" required>
-            <option value="" hidden disabled {{ $topic->id ? '' : selected }}>请选择分类</option>
+            <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类</option>
             @foreach($categories as $value)
               <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
                 {{ $value->name }}
