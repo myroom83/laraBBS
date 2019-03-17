83ffefb75c0ec76c4899bc728b09fb58d4939988
jeff sun <jeff.myroom83@gmail.com>
2019-03-17 20:49:46 +0800

    集成 laravel-permission 扩展包

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -49,6 +49,13 @@ class UsersTableSeeder extends Seeder
         $user->avatar = 'http://larabbs.test/uploads/images/avatars/201903/07/1_1551965175_8wUgzszAhw.jpg';
         $user->save();
 
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
+
 
     }
 }
