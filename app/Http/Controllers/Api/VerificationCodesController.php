072542fec292e69baf242297ff2ce1eaa3007703
jeff sun <jeff.myroom83@gmail.com>
2019-03-21 16:32:03 +0800

    user can register using phone

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -12,6 +12,7 @@ class VerificationCodesController extends Controller
     {
         $phone = $request->phone;
 
+        // 生成4位随机数，左侧补0
         if (!app()->environment('production')) {
             $code = '1234';
         } else {
@@ -27,6 +28,15 @@ class VerificationCodesController extends Controller
                 return $this->response->errorInternal($message ?: '短信发送异常');
             }
         }
+
+        $key = 'verificationCode_'.str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
     }
 }
-
